5fdd1cced35655da678c0dabd0cbff20004ef16b
Ceciliaolabode <60604394+Ceciliaolabode@users.noreply.github.com>
2023-02-21 02:06:53 +0100

    Add files via upload

---
+++ FirstProgram.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<h1>First Program</h1>
+</head>
+<body>
+   <?php
+        print "Running my First Program in php with XAMPP Server."
+    ?>
+</body>
+
+</html>
---
+++ tryit.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>  
+<head>
+
+</head>
+
+<php> 
+<h2>This is using include.</h2>
+<h3>Fantabulous!</h3>
+<?php 
+include ('neewww.php');
+ ?>
+</body>
+</html>
